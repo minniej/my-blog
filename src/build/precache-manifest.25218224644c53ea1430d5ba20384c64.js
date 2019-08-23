0b5d35bfe4fdd94bfcb3217d1a961c50f1ac88db
MINNIE <minniejung11@gmail.com>
2019-08-23 14:27:12 -0400

    first commit

---
+++ src/build/precache-manifest.25218224644c53ea1430d5ba20384c64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a8f57d3a6bc757816fafbc0b576f80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b273f9286e3180c983e8",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "49c25252228585af81e6",
+    "url": "/static/js/2.3e78a732.chunk.js"
+  },
+  {
+    "revision": "b273f9286e3180c983e8",
+    "url": "/static/js/main.2dbfd86c.chunk.js"
+  },
+  {
+    "revision": "590b1f1a44e44eae64fb",
+    "url": "/static/js/runtime~main.8f66630e.js"
+  }
+]);
